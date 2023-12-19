c9ee8a5f099d9e3818861b62fce6f93bd9a75b78
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2023-12-19 16:30:38 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name = "old";
+    public String getName() {
+        return this.Name;
+    }
+    public void setName(String variable) {
+        this.Name = variable;
+    }
 }
